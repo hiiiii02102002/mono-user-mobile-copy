bac317a13f008e18b17564f543f400962e3d6bf4
hiiiii02102002 <162083694+hiiiii02102002@users.noreply.github.com>
2024-05-29 10:42:07 +0700

    update

--- workbox-config.js
+++ workbox-config.js
@@ -5,5 +5,42 @@ module.exports = {
     globIgnores: [],
     ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
     swDest: 'www/service-worker.js',
-    
+    runtimeCaching: [
+        {
+            urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+            handler: 'CacheFirst',
+            options: {
+                cacheName: 'images',
+                expiration: {
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                },
+            },
+        },
+        {
+            urlPattern: /\.(?:js|css)$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+                cacheName: 'static-resources',
+            },
+        },
+        {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+                cacheName: 'google-fonts-stylesheets',
+            },
+        },
+        {
+            urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+            handler: 'CacheFirst',
+            options: {
+                cacheName: 'google-fonts-webfonts',
+                expiration: {
+                    maxEntries: 30,
+                    maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
+                },
+            },
+        },
+    ],
 };
